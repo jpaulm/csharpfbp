f7db1ca8f7030fb610d6a754389bb6d1d626ee8e
J Paul Morrison <jpaulmorr@gmail.com>
2014-08-25 12:46:16 -0400

    Uploading new application; restructuring Concord code slightly

---
+++ Concord/NetDefn.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FBPLib;
+using Components;
+using System.Windows.Forms;
+
+namespace Concord
+{
+    class NetDefn : Network
+    {
+       public override void Define()
+        {
+            // StringWriter SwOut = new StringWriter();
+            //string Filename = @"..\..\..\concord.txt";
+            Component("Dir1", typeof(DirList));
+            Component("Match", typeof(Match));
+            Component("Show3", typeof(WriteTextBox));
+            Component("Read", typeof(ReadStreamBlob));
+            Component("Clean", typeof(CleanBlob));
+            Component("Words", typeof(TextToWords));
+            Component("DisplayWords", typeof(DisplayWords));
+            Component("Sort", typeof(Sort));
+            Component("Format", typeof(FormatConcord));
+            Component("Show1", typeof(WriteTextBox));
+            Component("Show2", typeof(WriteTextBox));
+
+            Initialize(@"..\..\..\", "Dir1.IN");
+            Initialize(@"fake_cy", "Match.CONFIG");
+            Initialize(@"5,50", "Format.CONFIG");
+            TextBox tb = parms[0] as TextBox;
+            TextBox tb2 = parms[1] as TextBox;
+            TextBox tb3 = parms[2] as TextBox;
+            Initialize(tb, "Show1.DESTINATION");
+            Initialize(tb2, "Show2.DESTINATION");
+            Initialize(tb3, "Show3.DESTINATION");
+
+            Connect("Dir1.OUTF", "Match.IN");
+            Connect("Match.OUT", "Show3.IN");
+            Connect("Show3.OUT", "Read.IN");
+            Connect("Read.OUT", "Clean.IN");
+            Connect("Clean.OUT", "Words.IN");
+            Connect("Words.OUT", "DisplayWords.IN");
+            Connect("DisplayWords.OUT", "Sort.IN");
+            Connect("DisplayWords.OUTD", "Show1.IN");
+            Connect("Sort.OUT", "Format.IN");
+            Connect("Format.OUT", "Show2.IN");
+
+            Component("Discard1", typeof(Discard));
+            Component("Discard2", typeof(Discard));
+            Component("Discard3", typeof(Discard));
+            Component("Discard4", typeof(Discard));
+            Connect("Dir1.OUT", "Discard1.IN");
+            Connect("Dir1.OUTD", "Discard2.IN");
+            Connect("Match.OUTN", "Discard3.IN");
+            Connect("Words.OUTN", "Discard4.IN");
+            Connect("Show1.OUT", "Discard4.IN");
+            Connect("Show2.OUT", "Discard4.IN");
+
+
+        }
+    }
+    }
+
---
+++ TestNetworks/MergeAndSort/MergeAndSort.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using Components;
+using FBPLib;
+
+
+namespace TestNetworks
+{
+    public class MergeAndSort : Network
+    {
+
+        /* *
+          * Copyright 2007, 2008, J. Paul Morrison.  At your option, you may copy, 
+          * distribute, or make derivative works under the terms of the Clarified Artistic License, 
+          * based on the Everything Development Company's Artistic License.  A document describing 
+          * this License may be found at http://www.jpaulmorrison.com/fbp/artistic2.htm. 
+          * THERE IS NO WARRANTY; USE THIS PRODUCT AT YOUR OWN RISK.
+          * */
+
+        public override void Define() /* throws Throwable */ {
+
+
+            Component("Generate", typeof(Generate));
+            Component("Passthru", typeof(Passthru));
+            Component("Passthru2", typeof(Passthru));
+            Component("Sort", typeof(Sort));
+            Component("Write", typeof(WriteText));
+
+            Connect(Component("Generate"),
+                Port("OUT"),
+                Component("Passthru"),
+                Port("IN"));
+
+            Connect("Passthru2.OUT", "Passthru.IN");
+            Initialize("60",
+                Component("Generate"),
+                Port("COUNT"));
+
+            Connect(Component("Generate2", typeof(Generate)),
+                Port("OUT"),
+                Component("Passthru2"),
+                Port("IN"));
+
+            Connect("Passthru.OUT", "Sort.IN");
+
+            Initialize("75",
+                Component("Generate2"),
+                Port("COUNT"));
+
+            Connect(Component("Sort"),
+                Port("OUT"),
+                Component("Write"),
+               Port("IN"));
+
+            Stream st = Console.OpenStandardOutput();
+            Initialize(st,
+                Component("Write"),
+                Port("DESTINATION"));
+
+            Initialize("-",
+                Component("Write"),
+                Port("FLUSH"));
+            Initialize("500",
+                Component("Write"),
+                Port("CONFIG"));
+
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            (new MergeAndSort()).Go();
+        }
+        
+    }
+}
+
+
