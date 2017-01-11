7afc223e0e9f34acc11386f03cb6b9734a48fef6
johan <johan@DESKTOP-LHMFSQ8>
2017-01-10 20:19:57 -0800

    Start Network as a task. This allows optionally blocking on Go() using async/await.

---
+++ UnitTests/ReadTextTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FBPLib;
+using System.Linq;
+using Components;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CanReadText : Network
+    {
+        /* *
+               * Copyright 2007, 2008, J. Paul Morrison.  At your option, you may copy, 
+               * distribute, or make derivative works under the terms of the Clarified Artistic License, 
+               * based on the Everything Development Company's Artistic License.  A document describing 
+               * this License may be found at http://www.jpaulmorrison.com/fbp/artistic2.htm. 
+               * THERE IS NO WARRANTY; USE THIS PRODUCT AT YOUR OWN RISK.
+               * */
+
+        public StoreValues StoreValues;
+
+        public override void Define() /* throws Throwable */
+        {
+            // component("MONITOR", Monitor.class);
+            var assertions =
+                Enumerable.Range(0, 100)
+                          .Select(x => new Func<string, bool>(y => String.Format("{0:d6}abcd", 100 - x) == y))
+                          .ToArray();
+
+            Connect(Component("Read", typeof(ReadText)),
+                Port("OUT"),
+                Component("StoreValues", typeof(StoreValues)),
+                Port("IN"));
+
+            StoreValues = Component("StoreValues") as StoreValues;
+
+            Object d = (Object)@"fake_latin.txt";
+            Initialize(d,
+                Component("Read"),
+                Port("SOURCE"));
+        }
+
+        [TestMethod]
+        public async Task CanReadTextTest()
+        {
+            var network = new CanReadText();
+            await network.Go();
+            var values = network.StoreValues.Values.Cast<string>().ToArray();
+            Assert.AreEqual(1, values.Length);
+            Assert.IsTrue(values[0].StartsWith("Lorem ipsum"));
+        }
+    }
+}
---
+++ UnitTests/StoreValues.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FBPLib;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    /** 
+    * Component to compare the input of the component with an expected value.
+    */
+
+    [InPort("IN")]
+    [OutPort("OUT", optional = true)]
+    [ComponentDescription("Store values of all packets received")]
+    public class StoreValues : Component
+    {
+        internal static string _copyright =
+                "Copyright 2007, 2008, 2009, J. Paul Morrison.  At your option, you may copy, " +
+                "distribute, or make derivative works under the terms of the Clarified Artistic License, " +
+                "based on the Everything Development Company's Artistic License.  A document describing " +
+                "this License may be found at http://www.jpaulmorrison.com/fbp/artistic2.htm. " +
+                "THERE IS NO WARRANTY; USE THIS PRODUCT AT YOUR OWN RISK.";
+
+
+        IInputPort _inport;
+        List<object> _values = new List<object>();
+
+        public IReadOnlyList<object> Values => _values;
+
+        public override void OpenPorts()
+        {
+            _inport = OpenInput("IN");
+        }
+
+        public override void Execute()
+        {
+            Packet p;
+            while ((p = _inport.Receive()) != null)
+            {
+                if (p.Type != Packet.Types.Open && p.Type != Packet.Types.Close)
+                {
+                    _values.Add(p.Content);
+                }
+
+                Drop(p);
+            }
+        }
+    }
+}
